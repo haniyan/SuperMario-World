a6a9a973f41d22731131b94a23c332a623260aae
haniyan <hania-nocun@o2.pl>
2022-04-09 15:32:03 +0200

    typical animation added to text

--- my-app/src/js/components/elements/MainSection.js
+++ my-app/src/js/components/elements/MainSection.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import MainDiv from '../../../assets/markers/main_div.png'
-import FavBtn from '../../../assets/markers/fav_btn.png'
+import React, {useState} from "react";
+import Typical from "react-typical";
+import MainDiv from '../../../assets/markers/main_div.png';
+import FavBtn from '../../../assets/markers/fav_btn.png';
+
 
 
 
 export const MainSection = () => {
+//STATE DO INFOCONTENT
 
-    const text = "CO ROBIĆ WE WROCŁAWIU?"
+    const [color, setColor] =useState('#0e8d63')
+    // const [text, setText] =useState(["CO ROBIĆ WE WROCŁAWIU?"])
+
+//FUNKCJE INFOCONTENT
+    function changeTextColorInfoContent(){
+        setColor(setColor=> '#e34864')
+    }
+    const infoContentAnimation=()=>{
+
+        setTimeout(()=>{
+            changeTextColorInfoContent()
+
+        },7000)
+
+    }
+
+    //WYSWIETLANIE PO LITERCE - react typical
+
+
+
 
     return (
-        // <div className="mainSection" style={{backgroundImage: `url(${MainDiv})`}}>
+        infoContentAnimation(),
         <div className="mainSection">
-            <button className="addToFav" style={{backgroundImage: `url(${FavBtn})`}} />
+            <button className="addToFav" style={{backgroundImage: `url(${FavBtn})`}}/>
             <div className="icon"/>
-            <h1 className="infoContent" >{text}</h1>
+            <h1 className="infoContent" style={{color: `${color}`, fontSize: "40px", textAlign: "center"}}>
+            <Typical
+                loop = {Infinity}
+                wrapper="b"
+                steps={
+                    ["CO ROBIĆ WE WROCŁAWIU?",
+                        7000,
+                        "WYBIERZ LOSOWE MIEJSCE NA MAPIE",
+                        7000,
+                    ]
+                }/>
+
+            </h1>
             <div className="infoBox" style={{backgroundImage: `url(${MainDiv})`}}/>
         </div>
     );
