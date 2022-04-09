8066904afcb95be51c80a88aa27c34d9eca7faf1
haniyan <hania-nocun@o2.pl>
2022-04-09 14:18:09 +0200

    added animation for text

--- my-app/src/js/components/LeftColumn.js
+++ my-app/src/js/components/LeftColumn.js
@@ -9,27 +9,13 @@ export const LeftColumn = () => {
         <>
             <div className="leftColumn">
                 <div className="mapSection">
-                    <MapSection     googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-
+                    <MapSection     googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC5wLdzlHyrigrwHViGuOgRspZk9udmIac&map_ids=2aa77f8c10881da9&v=3.exp&libraries=geometry,drawing,places"
                                     loadingElement={<div style={{ height: `100%` }} />}
-                                    containerElement={<div style={{ height: `400px`, width: "500px" }} />}
-                                    mapElement={<div style={{ height: `100%` }} />}/>    </div>
+                                    containerElement={<div style={{ height: `400px`, width: "850px" }} />}
+                                    mapElement={<div style={{ height: `100%` }} />}/>
+                </div>
                 <TipSection/>
             </div>
         </>
     )
 }
-
-//
-// export const LeftColumn = () => {
-//     return (
-//         <>
-//             <div className="leftColumn">
-//                 <div className="mapSection">
-//                     <div id="map"/>
-//                 </div>
-//                 <TipSection/>
-//             </div>
-//         </>
-//     )
-// }
--- my-app/src/js/components/elements/MainSection.js
+++ my-app/src/js/components/elements/MainSection.js
@@ -5,12 +5,15 @@ import FavBtn from '../../../assets/markers/fav_btn.png'
 
 
 export const MainSection = () => {
+
+    const text = "CO ROBIĆ WE WROCŁAWIU?"
+
     return (
         // <div className="mainSection" style={{backgroundImage: `url(${MainDiv})`}}>
         <div className="mainSection">
             <button className="addToFav" style={{backgroundImage: `url(${FavBtn})`}} />
             <div className="icon"/>
-            <h1 className="infoContent" >Co robić we Wrocławiu?</h1>
+            <h1 className="infoContent" >{text}</h1>
             <div className="infoBox" style={{backgroundImage: `url(${MainDiv})`}}/>
         </div>
     );
