cef345e5102d65de232179db787f807d4bbb2bf7
haniyan <hania-nocun@o2.pl>
2022-04-19 16:57:54 +0200

    changed sizes of icons

--- my-app/src/js/function/markerClick.js
+++ my-app/src/js/function/markerClick.js
@@ -1,9 +1,9 @@
-export const markerHandleClick = (marker) => {
-
-    const theme = marker.properties.id;
-
-    console.log(theme, typeof(theme))
-    console.log(marker.geometry.coordinates[0],marker.geometry.coordinates[1])
-
-    return theme
-}
+// export const markerHandleClick = (marker) => {
+//
+//     const theme = marker.properties.id;
+//
+//     console.log(theme, typeof(theme))
+//     console.log(marker.geometry.coordinates[0],marker.geometry.coordinates[1])
+//
+//     return theme
+// }
